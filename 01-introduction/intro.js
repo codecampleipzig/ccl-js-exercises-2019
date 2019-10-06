6d6c0e07b866ec9e43f0b40a77b8973f897579df
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-10-06 13:38:32 +0200

    Add intro example program

---
+++ 01-introduction/intro.js
@@ -0,0 +1,8 @@
+var myName = "Gabe";
+var myGithub = "https://github.com/gabrielheinrich";
+
+function introduce(name, url) {
+    console.log (`Hello, my name is ${name} and you can find me at ${url}.`);
+}
+
+introduce(myName, myGithub);
