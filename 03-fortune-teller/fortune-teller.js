eae82f734587fb584d6799f8fda47604d63446e2
GigaHierz <hierzilena@gmail.com>
2019-10-09 11:54:54 +0200

    deleted the space form the variable pluralNoun, like Gabe requested

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,6 +1,6 @@
 var noun = "scorpio";
 var adjective = "iridescent";
-var pluralNoun = "shimmering quests ";
+var pluralNoun = "shimmering quests";
 
 var message = `Those born under the planetary sign of the ` + noun + ` possess
 ` + adjective + ` personalities and are forever searching for new ` + pluralNoun + `
