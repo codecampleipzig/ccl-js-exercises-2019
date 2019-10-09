a674c8bdf06aee1c67792e54d2f90b393689ac2e
Nick <nlavery@hotmail.co.uk>
2019-10-09 12:26:59 +0200

    updated and simplified complement-color.js

--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -1,13 +1,9 @@
 var red = 75;
 var green = 125;
 var blue = 200;
-var cred = 255-red
-var cgreen = 255-green
-var cblue = 255-blue
-var color = [red,green,blue]
-var ccolor = [cred, cgreen, cblue]
-
-var a = 'The complement color of rgb ('
-var b = ') is rgb ('
-var c = ')'
+var color = [red,green,blue];
+var ccolor = [255-red, 255-green, 255-blue];
+var a = 'The complement color of rgb(';
+var b = ') is rgb(';
+var c = ')';
 console.log (a + color + b + ccolor + c);
