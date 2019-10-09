c8461b583b9c4ac76204cabb029c7034284034a2
Andres Cortes <andresmcg@gmail.com>
2019-10-09 11:57:38 +0200

    fix semicolon bug

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -4,4 +4,4 @@ var pluralNoun = "planets";
 
 var message = "Those born under the planetary sign of the " + noun + " possess " + adjective + " personalities and are forever searching for new " + pluralNoun + " to conquer.";
 
-console.log(message)
+console.log(message);
