39acc31d72f47b2963801f9a74b211f42d535687
[Nicolas Araneda Hinrichs] <[niko.ah@gmail.com]>
2019-10-09 12:08:36 +0200

    commiting semicolons

--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -1,6 +1,6 @@
-var red = 1
-var green = 2
-var blue = 3
-var color = [red+green+blue]
-var complementColor = [255-red, 255-green, 255-blue]
-console.log (complementColor)
+var red = 1;
+var green = 2;
+var blue = 3;
+var color = [red+green+blue];
+var complementColor = [255-red, 255-green, 255-blue;]
+console.log (complementColor);
