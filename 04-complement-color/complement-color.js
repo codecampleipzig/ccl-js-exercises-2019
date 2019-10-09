6cc92d9933718f29ec7fa6fb86b5f629ff7f5b47
Bea Medina <beabloo@protonmail.com>
2019-10-09 12:20:12 +0200

    Add complement-color

---
+++ 04-complement-color/complement-color.js
@@ -0,0 +1,6 @@
+var red = 70;
+var green = 0;
+var blue = 20;
+var complementColor = [255 - red, 255 - green, 255 - blue];
+
+console.log ("The complement color of rgb (" +red+","+green+","+blue + " )is rgb(" + complementColor + ").");
