12f1b9c0a8e75850d0b086bc43ff3b45310607ee
MGraf <info@moritz-graf.de>
2019-10-09 12:01:43 +0200

    Security enhancements and better error message.

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,12 +1,15 @@
-function random( randomOf = false ) {
+function random( randomOf ) {
     if (randomOf) {
         return randomOf[Math.floor(Math.random()*randomOf.length)]
+    } else {
+        console.error("No randomOf defined for random().")
     }
 }
 
 var noun = ["rat", "scorpion", "whatever"];
 var adjective = ["multiple", "some", "any"];
-var pluralNoun = ["ways","whatevers","nouns"];
+var pluralNoun = ["ways", "whatevers", "nouns"];
+
 var message = `Those born under the planetary sign of the ` + random(noun) + ` possess 
 ` + random(adjective) + ` personalities and are forever searching for new ` + random(pluralNoun) + ` 
 to conquer.`;
