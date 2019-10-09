3a2cb86fbbe2632fd781fb30c591413b243c8675
Nick <nlavery@hotmail.co.uk>
2019-10-09 11:49:33 +0200

    add fortune-teller

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,9 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
+var a = 'Those born under the planetary sign of the';
+var b = ' possess';
+var c = ' personalities and are forever searching for new';
+var d = ' to conquer.';
+var noun = ' croissant';
+var adjective = ' spicy'; 
+var pluralnoun = ' feet';
+
+console.log (a + noun + b + adjective + c + pluralnoun + d)
