cda5e77ab86d0d4a690b593d10889a8a2185b5a4
mariana moreira <m.camargomoreira@gmail.com>
2019-10-09 11:57:29 +0200

    new horoscope

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -8,4 +8,4 @@ var adjective = "charismatic";
 
 var plural = "lifelong fans";
 
-console.log('Those born under the planetary sign of the'+ ' ' + noun + ' ' + 'possess'+ ' ' + adjective + ' ' + 'personalities and are forever searching for new' + ' ' + plural + ' '+ 'to conquer.')
+console.log('Those born under the planetary sign of the ' + noun + ' possess ' + adjective + ' personalities and are forever searching for new ' + plural + ' to conquer.')
