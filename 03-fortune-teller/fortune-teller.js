cfdee645fb4a340c74cd2ed1ba9652325ccb2d6b
[Nicolas Araneda Hinrichs] <[niko.ah@gmail.com]>
2019-10-09 12:08:06 +0200

    Commiting colors

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,6 +1,6 @@
-var noun = "gemini"
-var adjective = "multiple"
-var pluralNoun = "empires"
+var noun = "gemini";
+var adjective = "multiple";
+var pluralNoun = "empires";
 var message = "Those born under the planetary sign of the " + noun + " possess "
 + adjective + " personalities and are forever searching for new " + pluralNoun +
 " to conquer.";
