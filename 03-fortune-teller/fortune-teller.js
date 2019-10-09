17c9721b1c7832435f0c1f0b9cc3773c6ff5c6bb
paulinedelphine <hannahpaulina@mailbox.org>
2019-10-09 11:53:02 +0200

    the magic planetary

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,8 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
+var noun = "pieces";
+var adjective = "sensitive";
+var pluralNoun = "lions";
+
+var message = "Those born under the planetary sign of the " + noun + " possess "
++ adjective + " personalities and are forever searching for new " + pluralNoun + " to conquer.";
+
+console.log(message)
