08b6a23d2c8569c3a9a171e5c93c34f9b13234ed
Burnilein <bjorn.seidel@gmail.com>
2019-10-09 11:56:25 +0200

    minus the arrays of variables (don't know randomizing yet

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,6 +1,6 @@
-var noun = ["Rat", "Horse", "Pig"];
-var adjective = ["lucky", "great", "superb"];
-var pluralNoun = ["worlds", "7 seas", "the hoods"];
+var noun = ["Rat"];
+var adjective = ["lucky"];
+var pluralNoun = ["worlds"];
 
 var message = "Those born under the planetary sign of the " + noun + " possess " + adjective + " personalities and are forever searching for new " + pluralNoun + "to conquer.";
 
