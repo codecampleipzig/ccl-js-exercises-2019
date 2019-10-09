b6a9806c5d8f0ca15b24541c2661d02775c3b5dc
Burnilein <bjorn.seidel@gmail.com>
2019-10-09 14:08:02 +0200

    should work this way now

---
+++ 04-complement-color/complement-color.js
@@ -0,0 +1,9 @@
+var red = 240
+var green = 0
+var blue = 100
+
+var redComp = 255 - red
+var greenComp = 255 - green
+var blueComp = 255 - blue 
+
+console.log("RGB " + redComp + ", " + greenComp + "," + blueComp)
