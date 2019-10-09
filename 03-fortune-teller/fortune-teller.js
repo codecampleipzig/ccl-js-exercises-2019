b0cd3e6e7fe6bc50aac6776acd3977477a85ed43
MGraf <info@moritz-graf.de>
2019-10-09 11:46:58 +0200

    Randomized Fortune Teller

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,14 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
+function random( randomOf = false ) {
+    if (randomOf) {
+        return randomOf[Math.floor(Math.random()*randomOf.length)]
+    }
+}
+
+var noun = ["Rat", "Wiesele", "blabla"];
+var adjective = ["multiple", "easy", "hard"];
+var pluralNoun = ["ways","whatevers","Moritzes"];
+var message = `Those born under the planetary sign of the ` + random(noun) + ` possess 
+` + random(adjective) + ` personalities and are forever searching for new ` + random(pluralNoun) + ` 
 to conquer.`;
+
+console.log(message);
