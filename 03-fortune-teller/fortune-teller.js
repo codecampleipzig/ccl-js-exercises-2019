6040cfdf9a14b2a432440938f108f3b1ed376a57
Burnilein <bjorn.seidel@gmail.com>
2019-10-09 11:52:54 +0200

    added fortune teller without randomization

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,7 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
+var noun = ["Rat", "Horse", "Pig"];
+var adjective = ["lucky", "great", "superb"];
+var pluralNoun = ["worlds", "7 seas", "the hoods"];
+
+var message = "Those born under the planetary sign of the " + noun + " possess " + adjective + " personalities and are forever searching for new " + pluralNoun + "to conquer.";
+
+console.log(message)
