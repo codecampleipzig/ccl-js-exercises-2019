e6f6b720e3e6d61e49f7f5908f4550548a1b3e86
[Nicolas Araneda Hinrichs] <[niko.ah@gmail.com]>
2019-10-09 11:44:29 +0200

    Lena is a gemini

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,7 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
+var noun = "gemini"
+var adjective = "multiple"
+var pluralNoun = "empires"
+var message = "Those born under the planetary sign of the " + noun + " possess "
++ adjective + " personalities and are forever searching for new " + pluralNoun +
+" to conquer.";
+console.log (message)
