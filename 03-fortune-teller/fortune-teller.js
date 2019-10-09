8fbf35899754779c3b12f2a296e7231b91b550d1
Andres Cortes <andresmcg@gmail.com>
2019-10-09 11:54:50 +0200

    I believe in zodiac signs but with less words

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,11 +1,7 @@
-var noun = "the pisces"
-var adjective = "saint seiya"
-var pluralNoun = "planets"
+var noun = "the pisces";
+var adjective = "saint seiya";
+var pluralNoun = "planets";
 
-var message = String("Those born under the planetary sign of the ") + noun + 
-String(" possess ") + adjective + 
-String(" personalities and are forever searching for new ") + pluralNoun + 
-String(" to conquer.");
-
+var message = "Those born under the planetary sign of the " + noun + " possess " + adjective + " personalities and are forever searching for new " + pluralNoun + " to conquer.";
 
 console.log(message)
