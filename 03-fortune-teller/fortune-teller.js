e1ea95345f33907c9d0380b9e95084c057077dd9
Bea Medina <beabloo@protonmail.com>
2019-10-09 11:58:24 +0200

    Update fortune-teller

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,10 +1,6 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
-
 var horoscope = "Capricorn";
 var randomAdjetive = "rebel";
-var randomPluralNoun = "universes"
+var randomPluralNoun = "universes";
 
 console.log("Those born under the planetary sign of the " + horoscope + " possess "
 + randomAdjetive + " personalities and are forever searching for new " + randomPluralNoun +
