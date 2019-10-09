06dbf28f342068df9acb28020819c97c59112322
Andres Cortes <andresmcg@gmail.com>
2019-10-09 12:25:04 +0200

    add color complement

---
+++ 04-complement-color/complement-color.js
@@ -0,0 +1,16 @@
+var red = 100;
+var green = 40;
+var blue = 0;
+
+/// var color = [red, green, blue];
+
+var cRed = 255-red;
+var cGreen = 255-green;
+var cBlue = 255-blue;
+
+///var complement = [cRed, cGreen, cBlue]
+
+console.log("Complement of red is " + cRed + ". " +
+"Complement of green is " + cGreen + ". " +
+"Complement of blue is " + cBlue + "."
+);
