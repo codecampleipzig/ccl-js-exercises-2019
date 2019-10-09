16b96405c97cdaf61d9c322d5220734a6745516d
GigaHierz <hierzilena@gmail.com>
2019-10-09 11:44:52 +0200

    Update file fortune teller to tell about scorpios future like nicos

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,3 +1,9 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
+var noun = "scorpio";
+var adjective = "iridescent";
+var pluralNoun = "shimmering quests ";
+
+var message = `Those born under the planetary sign of the ` + noun + ` possess
+` + adjective + ` personalities and are forever searching for new ` + pluralNoun + `
 to conquer.`;
+
+console.log(message);
