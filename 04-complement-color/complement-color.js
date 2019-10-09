b4ed4ab8bf8556911caeb7b38c9f5e36a385d6b4
GigaHierz <hierzilena@gmail.com>
2019-10-09 12:20:10 +0200

    changed complement-color, so it will output the complement color to a color

---
+++ 04-complement-color/complement-color.js
@@ -0,0 +1,12 @@
+var red = 0;
+var green = 1;
+var blue = 2;
+
+var complementRed = 255 - red;
+var complementGreen = 255 -green;
+var complementBlue = 255 - blue;
+
+var color = [red, green, blue];
+var complementColor = [complementRed, complementGreen, complementBlue];
+
+console.log("The complement color of rgb (" + color + ") is rgb(" + complementColor + ").")
