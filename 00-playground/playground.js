97ecc8976d4a6801e7c40e53cc8626df7a22691a
Bea Medina <beabloo@protonmail.com>
2019-10-10 10:39:38 +0200

    Add playground.js

---
+++ 00-playground/playground.js
@@ -0,0 +1,18 @@
+// var favoriteCake = "carrot cake"
+// if (favoriteCake == "carrot cake") {
+//     console.log ("Let's bake a " + favoriteCake);
+// } 
+// else if (favoriteCake == "cheese cake") {
+//     console.log ("Let's bake a " + favoriteCake);
+// } 
+// else {
+//     console.log ("There is nothing I can do for you.");
+// }
+
+// console.log ("Done");
+
+if (((process.argv).length) > 2){
+    console.log (process.argv);
+} else {
+    console.log ("Not valid.");
+}
--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -1,6 +1,15 @@
-var red = 70;
-var green = 0;
-var blue = 20;
-var complementColor = [255 - red, 255 - green, 255 - blue];
+var color = {
+    red : 70,
+    green : 0,
+    blue : 20
+};
 
-console.log ("The complement color of rgb (" +red+","+green+","+blue + " )is rgb(" + complementColor + ").");
+var complement = {
+    red : 255 - color.red,
+    green : 255 - color.green,
+     blue : 255 - color.blue
+}; 
+var colorString = "rgb(" + color.red + "," + color.green + "," + color.blue + ")"; 
+var complementColorString =" rgb t(" + complement.red + "," + complement.green + "," + complement.blue + ")";
+
+console.log ("The complement color of " + colorString + "is" + complementColorString + ".");
