d0c857b5780f95304d8c429811b86cf6cbf99a94
mariana moreira <m.camargomoreira@gmail.com>
2019-10-10 10:37:40 +0200

    mariana's command line argv

---
+++ 00-playground/playground.js
@@ -0,0 +1,9 @@
+
+
+if (typeof (process.argv)[2] == typeof "") {
+    console.log("I got the command line arg " + process.argv[2]);
+} else {
+    console.log("Something went wrong.")
+}
+
+
---
+++ 05-command-line-args/command-line-args.js
@@ -0,0 +1,7 @@
+
+
+if (typeof (process.argv)[2] == typeof "") {
+    console.log("I got the command line arg " + process.argv[2]);
+} else {
+    console.log("Something went wrong.")
+}
