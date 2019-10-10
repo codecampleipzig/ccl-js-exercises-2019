1b9277bd54e199c21f720754e684e3735885e98c
Nick <nlavery@hotmail.co.uk>
2019-10-10 22:36:49 +0200

    add tree.js

---
+++ 06-triangle/tree.js
@@ -0,0 +1,34 @@
+var depth = 5;
+
+for (i = 0; i < depth; i++) {
+
+var space = ""
+    for (j = 0; j < i+1; j++) {
+        space = space + " ";
+    }
+}
+
+console.log (space + " #");
+
+for (k = 0; k < depth; k++) {
+
+    var tree = "#"
+    
+        for (l = 0; l < k+1; l++) {
+        tree = tree + "##";
+        space = space.substring (0, depth-k);   
+        }
+    console.log (space + tree);
+}
+
+for (m = 0; m < depth; m++) {
+
+    var space = ""
+        for (n = 0; n < m+1; n++) {
+            space = space + " ";
+        }
+    }
+
+    console.log (space + "| |");
+    console.log (space + "| |");
+    
--- 06-triangle/triangle copy.js
+++ 06-triangle/triangle copy.js
@@ -3,8 +3,9 @@ var depth = 10;
 for (i = 0; i < depth; i++) {
     var line = ""
     for (j = 0; j < i+1; j++) {
+        
         line = line + "#";
-
+        
     }
     console.log (line)
 }
