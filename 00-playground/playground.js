10795d252853e4c41e600f39e84a0bbd84d8bdb9
Simona Ilievska <s.ilievska.10@gmail.com>
2019-10-10 10:48:57 +0200

    Modified playground js - fixed formatting

--- 00-playground/playground.js
+++ 00-playground/playground.js
@@ -1,6 +1,5 @@
-
-if (process.argv.length>2) {
-    console.log ("There is value in process.argv[2] and it is: "+process.argv[2])
+if (process.argv.length > 2) {
+    console.log("There is value in process.argv[2] and it is: " + process.argv[2]);
 } else {
-    console.log("There is no process.argv[2]!")
+    console.log("There is no process.argv[2]!");
 }
