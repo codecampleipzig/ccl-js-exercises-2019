3d829e48cce6c8efa47ffc776ebb759a3f59d1c8
Andres Cortes <andresmcg@gmail.com>
2019-10-10 09:59:23 +0200

    add comments

--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -2,7 +2,11 @@ var red = 100;
 var green = 40;
 var blue = 0;
 
-/// var color = [red, green, blue];
+/// var color = {
+///     red : 150,
+///     green : 200,
+///     blue : 50 
+/// };
 
 var cRed = 255-red;
 var cGreen = 255-green;
@@ -10,7 +14,5 @@ var cBlue = 255-blue;
 
 ///var complement = [cRed, cGreen, cBlue]
 
-console.log("Complement of red is " + cRed + ". " +
-"Complement of green is " + cGreen + ". " +
-"Complement of blue is " + cBlue + "."
+console.log("Complement of red is " + cRed + ". " + "Complement of green is " + cGreen + ". " + "Complement of blue is " + cBlue + "."
 );
