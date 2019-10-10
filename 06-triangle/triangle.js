6fc8671497fa36aa14615ceb5aef68a3662a704c
GigaHierz <hierzilena@gmail.com>
2019-10-10 15:43:53 +0200

    Add triangle function

--- 06-triangle/triangle.js
+++ 06-triangle/triangle.js
@@ -1,10 +1,7 @@
 function triangle(string){
     var depth = 8;
     for (var i =0; i < depth; i++){
-        if (i == 0){
-            console.log("");
-        }
-        else if (i == 1){
+        if (i == 1){
             console.log(string);
         }
         else {
