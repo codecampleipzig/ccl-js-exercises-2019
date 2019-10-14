da7c01d74ebb389265f90ef69e5dcec9405c7cc0
Andres Cortes <andresmcg@gmail.com>
2019-10-14 14:20:53 +0200

    more syntax changes

--- 05-command-line-args/command-line-args.js
+++ 05-command-line-args/command-line-args.js
@@ -1,6 +1,6 @@
 if (process.argv[2] != "" && typeof(process.argv[2]) == "string") {
-    console.log("thank you, I like strings.");
+    console.log("you entered a string, thank you, I like strings.");
 }   
     else {
-    console.log("this is an empty string.");
+    console.log("you entered an empty string, this is considered an invalid input.");
 }
