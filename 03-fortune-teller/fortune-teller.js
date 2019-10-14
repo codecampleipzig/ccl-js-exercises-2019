ea0bfa7be4ddb3cc02a8bf4ef859a9a463db0af1
Nick <nlavery@hotmail.co.uk>
2019-10-14 14:21:35 +0200

    Fixed style errors

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,9 +1,7 @@
-var a = 'Those born under the planetary sign of the';
-var b = ' possess';
-var c = ' personalities and are forever searching for new';
-var d = ' to conquer.';
-var noun = ' croissant';
-var adjective = ' spicy'; 
-var pluralnoun = ' feet';
+var noun = 'croissant';
+var adjective = 'spicy'; 
+var pluralnoun = 'feet';
 
-console.log (a + noun + b + adjective + c + pluralnoun + d);
+console.log ('Those born under the planetary sign of the ' + noun + ' possess ' +
+adjective + ' personalities and are forever searching for new ' + pluralnoun + ' to conquer.');
+ 
--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -2,8 +2,6 @@ var red = 75;
 var green = 125;
 var blue = 200;
 var color = [red,green,blue];
-var ccolor = [255-red, 255-green, 255-blue];
-var a = 'The complement color of rgb(';
-var b = ') is rgb(';
-var c = ')';
-console.log (a + color + b + ccolor + c);
+var complement = [255-red, 255-green, 255-blue];
+
+console.log ('The complement color of rgb(' + color + ') is rbg(' + complement + ').');
