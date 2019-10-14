6df937328ece1172bc7c186bf1a21b44b2e1e582
[Nicolas Araneda Hinrichs] <[niko.ah@gmail.com]>
2019-10-14 14:10:56 +0200

    corrections

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,7 +1,9 @@
 var noun = "gemini";
 var adjective = "multiple";
 var pluralNoun = "empires";
+
 var message = "Those born under the planetary sign of the " + noun + " possess "
 + adjective + " personalities and are forever searching for new " + pluralNoun +
 " to conquer.";
-console.log (message)
+
+console.log (message);
