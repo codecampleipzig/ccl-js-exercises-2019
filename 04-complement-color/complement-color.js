71dcca203aa9381e082e05d52eb9b592b15f05c4
[Nicolas Araneda Hinrichs] <[niko.ah@gmail.com]>
2019-10-14 14:35:18 +0200

    latest corrections

--- 04-complement-color/complement-color.js
+++ 04-complement-color/complement-color.js
@@ -1,13 +1,13 @@
-var rgbColor {
+var rgbColor = {
    red : 1,
    green : 2,
    blue : 3
 };
 
 var compColor = {
-   255 - rgbColor.red,
-   255 - rgbColor.green,
-   255 - rgbColor.blue,
+   compRed : 255 - rgbColor.red,
+   compGreen : 255 - rgbColor.green,
+   compBlue : 255 - rgbColor.blue,
 };
 
 console.log ("The complement color of rgb ("+rgbColor.red+", "+rgbColor.green+", "+rgbColor.blue+") is rgb ("+compColor.red+", "+compColor.green+", "+compColor.blue+"};
