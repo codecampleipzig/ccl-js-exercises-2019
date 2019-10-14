3ebb927133c88751a91e1f816baf23cc7dcf63cf
Anahita Kazem <anahita.kazem@bio.ntnu.no>
2019-10-14 14:22:47 +0200

    Update fortune-teller script.

--- 03-fortune-teller/fortune-teller.js
+++ 03-fortune-teller/fortune-teller.js
@@ -1,7 +1,3 @@
-var message = `Those born under the planetary sign of the (noun) possess
-(adjective) personalities and are forever searching for new (plural noun)
-to conquer.`;
-
 var noun = "lion";
 var adjective = "glorious";
 var pluralNoun = "subjects";
