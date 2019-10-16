66277308d2f5b21b0febdc31b00f864332652db1
Irakli Shubitidze <iko95@gmx.de>
2019-10-16 11:01:00 +0200

    Solved the Array Task

--- 00-playground/playground.js
+++ 00-playground/playground.js
@@ -1,13 +1,25 @@
-function repeatStringNumTimes(string, times) {
-    var repeatedString = "";
-    while (times > 0) {
-    repeatedString += string;
-    times--;
-    }
-    return repeatedString;
+function checker(x){
+    return x == true;
 }
 
-for (var i = 0; i < 10; i++) {
-    var x = repeatStringNumTimes("#", i);
-    console.log(x);
+function eq(x,y){
+    if (Array.isArray(x) && Array.isArray(y)){
+        var ars = []; 
+        for  (var i = 0; i < x.length; i++){
+           ars.push(x[i] == y[i]); 
+        }
+        if (ars.every(checker)){
+        //if (ars[0] == true && ars[1] == true && ars[2] == true){
+            return true;
+        } 
+        else{
+            return false;
+        }
+    }
+    else {
+        return x == y;
+    }
 }
+
+console.log(eq(1, 1))
+console.log(eq([1,2,3],[1,2,4]))
