662daf577966de858d92a95d2f7abc8962abe34a
Irakli Shubitidze <iko95@gmx.de>
2019-10-16 14:05:10 +0200

    Updated the eq function

--- 00-playground/playground.js
+++ 00-playground/playground.js
@@ -3,10 +3,17 @@ function checker(x){
 }
 
 function eq(x,y){
-    if (Array.isArray(x) && Array.isArray(y)){
+    if (Array.isArray(x) && Array.isArray(y) && x.length == y.length){
         var ars = []; 
         for  (var i = 0; i < x.length; i++){
-           ars.push(x[i] == y[i]); 
+           if (Array.isArray(x[i]) && Array.isArray(y[i]) && x[i].length == y[i].length){
+               for (var l = 0; l < x[i].length; l++){
+                   ars.push(x[i[l]] == y[i[l]]);
+               }
+           }
+           else{
+               ars.push(x[i] == y[i]);
+           }
         }
         if (ars.every(checker)){
         //if (ars[0] == true && ars[1] == true && ars[2] == true){
@@ -22,4 +29,4 @@ function eq(x,y){
 }
 
 console.log(eq(1, 1))
-console.log(eq([1,2,3],[1,2,4]))
+console.log(eq([[1,2,3],2,3],[1,2,3]))
