68e1e69e5d953b27d527bf541d6f290574dc2239
Nick <nlavery@hotmail.co.uk>
2019-10-17 11:44:20 +0200

    Add changes from Wednesday

--- 10-functions/equal-nested.js
+++ 10-functions/equal-nested.js
@@ -1,4 +1,4 @@
-function eq(lhs, rhs) {
+function eq(lhs, rhs, recursive) {
 
     if (Array.isArray(lhs) && Array.isArray(rhs)) { 
         console.log ('First condition met: both are arrays');
@@ -7,23 +7,29 @@ function eq(lhs, rhs) {
             console.log ('Second condition met: both are same length');
 
             for (var i=0; i<lhs.length; i++) {
-                if (lhs[i] != rhs[i]){
+                if (Array.isArray(lhs[i]) && Array.isArray(rhs[i])) {
+                    console.log("Recursive Start");
+                    return eq (lhs[i], rhs[i], true);
+                }
+                else if (lhs[i] != rhs[i]){
                     console.log ('Index ' + i + ' of ' + (lhs.length-1) + ' not same: exit');
 
                     return false;
-                            
                 }
+
                 console.log ('Index ' + i + ' of ' + (lhs.length-1) + ' same');
 
             }
+            console.log ('All checks passed');
+            if ( recursive ){
+                return true;
+            }
         } 
-        console.log ('All checks passed');
-        return true;
+        console.log ("Not equal length: exit");
+        return false;
     } 
     console.log ('One or both not arrays: regular equality check');
     return lhs == rhs;
 }
 
-eq ([1, 2, 3], [1, 2, 3]);
-
- 
+eq ([1, 2 [1, 2, 3], 4], [1, 2, [1, 2, 3], 5])
